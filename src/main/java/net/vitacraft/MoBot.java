2d0261c322d98b4fb0150e4597c39532aa62f924
Relism <relismdev@gmail.com>
2024-11-22 15:59:16 +0100

    Reworked module loading: Added support for transitive dependencies.

--- src/main/java/net/vitacraft/MoBot.java
+++ src/main/java/net/vitacraft/MoBot.java
@@ -1,202 +1,196 @@
-package net.vitacraft.api;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
-import net.vitacraft.api.addons.SlashCommandAddon;
+package net.vitacraft;
+
+import net.dv8tion.jda.api.exceptions.InvalidTokenException;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import net.vitacraft.api.BotEnvironment;
+import net.vitacraft.api.MBModule;
+import net.vitacraft.api.PrimitiveBotEnvironment;
+import net.vitacraft.api.classloader.ModuleLoader;
 import net.vitacraft.api.config.ConfigLoader;
-import net.vitacraft.api.info.ModuleInfo;
-import net.vitacraft.api.info.StartUpPriority;
+import net.vitacraft.api.console.Console;
+import net.vitacraft.exceptions.BotStartupException;
+import net.vitacraft.manager.CommandManager;
+import net.vitacraft.api.console.ConsoleUtil;
 import org.simpleyaml.configuration.ConfigurationSection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import java.io.File;
+import java.util.*;
 
 /**
- * Represents a module within the bot,
- * encapsulating its configuration, lifecycle methods, and interactions with the bot environment.
+ * The main class for initializing and managing the MoBot application.
  * <p>
- * This class provides methods for module initialization, configuration, and interaction with the bot environment, including
- * registering commands and event listeners.
+ * This class handles the initialization of the bot, loading of modules,
+ * setting up the bot environment, and managing the lifecycle of the bot.
  * </p>
  */
-public class MBModule {
-    private BotEnvironment botEnvironment;
-    private final ModuleInfo moduleInfo;
+public class MoBot {
+    private final List<MBModule> modules = new ArrayList<>();
+    private final BotEnvironment botEnvironment;
     private final Logger logger;
-    private final ConfigLoader defaultConfig;
+    private Console console;
 
-    /**
-     * Constructs a new {@link MBModule} instance.
-     * <p>
-     * Initializes the module information, logger, and configuration loader.
-     * </p>
-     */
-    public MBModule(){
-        moduleInfo = retrieveModuleInfo();
-        logger = LoggerFactory.getLogger(moduleInfo.name());
-        defaultConfig = generateConfig("config.yml");
+    public MoBot() {
+        ConsoleUtil.clearConsole();
+
+        // Initialize the Logger
+        logger = LoggerFactory.getLogger("MoBot");
+
+        // Generate the DefaultShardManagerBuilder without initializing it
+        DefaultShardManagerBuilder builder = getBuilder();
+
+        // Set up the PrimitiveBotEnvironment and pass in all data available pre enabling
+        PrimitiveBotEnvironment primitiveBotEnvironment = new PrimitiveBotEnvironment(builder, this);
+
+        // Create the modules directory if it does not exist
+        createModulesDirectory();
+
+        // Load all modules
+        modules.addAll(ModuleLoader.loadModules(System.getProperty("user.dir") + "/modules"));
+        logger.info("Loaded MoBot modules: {}", modules.size());
+
+        // Sort modules based on priority
+        modules.sort(Comparator.comparing(module -> module.getModuleInfo().priority()));
+
+        // Call the preEnable method on all Modules
+        List<String> enabledModules = new ArrayList<>();
+        for (MBModule module : modules) {
+            try {
+                module.preEnable(primitiveBotEnvironment);
+                enabledModules.add(module.getModuleInfo().name());
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
+        logger.info("Pre-enabled modules: {}", enabledModules);
+
+        // Start the bot and construct the ShardManager
+        ShardManager shardManager;
+        try {
+            shardManager = enableBot(builder);
+            logger.info("Successfully enabled shard manager with {} shards.", shardManager.getShardsTotal());
+        } catch (BotStartupException e) {
+            botEnvironment = null;
+            logger.error("Bot startup failed: " + e.getMessage());
+            return;
+        }
+
+        // Initialize the CommandManager
+        CommandManager commandManager = new CommandManager();
+
+        // Set up the BotEnvironment
+        botEnvironment = new BotEnvironment(shardManager, this, commandManager);
+
+        // Register the CommandManager
+        shardManager.addEventListener(commandManager);
+
+        // Call the onEnable method on all Modules
+        for (MBModule module : modules) {
+            module.setBotEnvironment(botEnvironment);
+            try {
+                module.onEnable();
+                logger.info("Successfully Enabled module {}", module.getModuleInfo().name() + " by " + module.getModuleInfo().authors());
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
+
+        // Initialize the Console
+        console = new Console(this);
     }
 
-    /**
-     * Retrieves module information from the module's configuration file.
-     *
-     * @return a {@link ModuleInfo} instance containing the module's metadata
-     */
-    private ModuleInfo retrieveModuleInfo() {
-        ConfigLoader configLoader = new ConfigLoader(this.getClass(), "module.yml");
+    private DefaultShardManagerBuilder getBuilder() {
+        ConfigLoader configLoader = new ConfigLoader("./bot.yml");
+        configLoader.save();
         ConfigurationSection config = configLoader.getConfig();
-        String name = config.getString("name");
-        String version = config.getString("version");
-        String description = config.getString("description");
-        List<String> authors = config.getStringList("authors");
-        List<String> dependencies = config.getStringList("dependencies");
-        StartUpPriority startUpPriority;
+        String token = config.getString("token");
+        List<String> gateWayIntents = config.getStringList("gateway-intents");
 
-        try {
-            startUpPriority = StartUpPriority.valueOf(config.getString("priority"));
-        } catch (IllegalArgumentException e) {
-            startUpPriority = StartUpPriority.DEFAULT;
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+
+        for (String intent : gateWayIntents) {
+            builder.enableIntents(GatewayIntent.valueOf(intent));
         }
 
-        return new ModuleInfo(name, version, description, authors, dependencies, startUpPriority);
+        return builder;
     }
 
-    /**
-     * Generates and saves the module's configuration file.
-     *
-     * @return a {@link ConfigLoader} instance for the module's configuration
-     */
-    private ConfigLoader generateConfig(String resourceName) {
-        try {
-            Path configDir = Paths.get("modules" + "/" + moduleInfo.name());
-            Files.createDirectories(configDir);
-            ConfigLoader configLoader = new ConfigLoader(this.getClass(), resourceName, configDir);
+    private ShardManager enableBot(DefaultShardManagerBuilder builder) throws BotStartupException {
+        ShardManager shardManager = null;
+        Scanner scanner = new Scanner(System.in);
+
+        ConfigLoader configLoader = new ConfigLoader("./bot.yml");
+        ConfigurationSection config = configLoader.getConfig();
+        String token = config.getString("token");
+
+        if (token == null || token.isEmpty()) {
+            ConsoleUtil.print("No Discord Bot-Token found. This might be your first time running the bot. Please enter a valid bot token: ");
+            token = scanner.nextLine();
+            config.set("token", token);
             configLoader.save();
-            return configLoader;
-        } catch (Exception e) {
-            logger.error("Failed to generate configuration", e);
+            builder.setToken(token);
         }
-        return null;
+
+        while (shardManager == null) {
+            try {
+                shardManager = builder.build();
+            } catch (InvalidTokenException e) {
+                logger.info("The provided Discord Bot-Token is invalid. Please enter a new token: ");
+                String newToken = scanner.nextLine();
+                config.set("token", newToken);
+                configLoader.save();
+                builder.setToken(newToken);
+            } catch (Exception e) {
+                throw new BotStartupException("An unknown error occurred while setting up the shard manager.", e);
+            }
+        }
+
+        return shardManager;
     }
 
-    /**
-     * Called before the bot is enabled.
-     * <p>
-     * This method is intended to be overridden by subclasses to perform any necessary initialization
-     * before the module is enabled.
-     * </p>
-     *
-     * @param primitiveBotEnvironment the bot environment provided for the module's pre-enabling setup
-     */
-    public void preEnable(PrimitiveBotEnvironment primitiveBotEnvironment){
-        // To be overridden by subclasses
+    private void createModulesDirectory() {
+        File modulesDir = new File("modules");
+        if (!modulesDir.exists()) {
+            boolean created = modulesDir.mkdirs();
+            if (created) {
+                logger.info("Created modules directory.");
+            } else {
+                logger.error("Failed to create modules directory.");
+            }
+        }
     }
 
-    /**
-     * Called after the bot is disabled.
-     * <p>
-     * This method is intended to be overridden by subclasses to perform any necessary actions
-     * when the module is disabled.
-     * </p>
-     */
-    public void onEnable(){
-        // To be overridden by subclasses
+    public void shutdown() {
+        logger.info("Shutting down MoBot...");
+
+        for (MBModule module : modules) {
+            module.onDisable();
+        }
+
+        if (botEnvironment != null && botEnvironment.getShardManager() != null) {
+            botEnvironment.getShardManager().shutdown();
+            logger.info("Shard manager has been shut down.");
+        }
+
+        for (MBModule module : modules) {
+            module.postDisable();
+        }
+
+        logger.info("See you soon!.");
     }
 
-    /**
-     * Called before the bot is disabled.
-     * <p>
-     * This method is intended to be overridden by subclasses to perform any necessary cleanup
-     * after the module has been disabled.
-     * </p>
-     */
-    public void onDisable(){
-        // To be overridden by subclasses
-    }
-
-    /**
-     * Called after the bot is disabled.
-     * <p>
-     * This method is intended to be overridden by subclasses to perform any necessary cleanup
-     * after the module has been disabled.
-     * </p>
-     */
-    public void postDisable(){
-        // To be overridden by subclasses
-    }
-
-    /**
-     * Sets the {@link BotEnvironment} for this module.
-     *
-     * @param botEnvironment the {@link BotEnvironment} to be set
-     */
-    public void setBotEnvironment(BotEnvironment botEnvironment) {
-        this.botEnvironment = botEnvironment;
-    }
-
-    /**
-     * Registers a slash command with the bot's command manager.
-     *
-     * @param data the {@link CommandData} for the slash command
-     * @param addon the {@link SlashCommandAddon} to handle the slash command
-     */
-    public void registerSlashCommand(CommandData data, SlashCommandAddon addon){
-        botEnvironment.getCommandManager().registerCommand(data, addon);
-    }
-
-    /**
-     * Registers event listeners with the bot's shard manager.
-     *
-     * @param listeners the event listeners to be registered
-     */
-    public void registerEventListener(Object... listeners){
-        botEnvironment.getShardManager().addEventListener(listeners);
-    }
-
-    /**
-     * Returns the {@link BotEnvironment} for this module.
-     *
-     * @return the {@link BotEnvironment} instance
-     */
-    public BotEnvironment getBotEnvironment() {
-        return botEnvironment;
-    }
-
-    /**
-     * Returns the {@link ModuleInfo} for this module.
-     *
-     * @return the {@link ModuleInfo} instance
-     */
-    public ModuleInfo getModuleInfo() {
-        return moduleInfo;
-    }
-
-    /**
-     * Returns the {@link Logger} for this module.
-     *
-     * @return the {@link Logger} instance
-     */
     public Logger getLogger() {
         return logger;
     }
 
-    /**
-     * Returns the {@link ConfigLoader} for this module.
-     *
-     * @return the {@link ConfigLoader} instance
-     */
-    public ConfigLoader getConfigLoader() {
-        return defaultConfig;
+    public Console getConsole() {
+        return console;
     }
 
-    /**
-     * Returns a {@link ConfigLoader} instance for the specified resource file.
-     *
-     * @param resourceName the name of the resource file (e.g., "options.yml").
-     * @return a {@link ConfigLoader} instance for the specified resource file
-     */
-    public ConfigLoader getConfigLoader(String resourceName) {
-        return generateConfig(resourceName);
+    public static void main(String[] args) {
+        MoBot bot = new MoBot();
+        Runtime.getRuntime().addShutdownHook(new Thread(bot::shutdown));
     }
 }
